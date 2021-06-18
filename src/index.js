005b756ad3ff26f65ffdda51bd8b49f20171ef2d
AbsurdCoder <absurdcoders@gmail.com>
2021-06-18 20:25:04 +0100

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<>
+<div className="cards">
+  <div className="card">
+    <img src="https://occ-0-1217-1218.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABexf5k-uagTAnO2W57VCeMOdM_x9BImT0FEYglDzxQpzSxl7Qk6czu88nNuzHaDQ0mYQlR_9k08e4GWMmrQ53MG89y9voICbiD-Jv3_unXVww--olj2XmxJ35Wny.jpg?r=260" alt="" className="card_img"/>
+    <div className="card_info">
+      <span className="card_category"> Netflix Original </span>
+      <h3 className="card_title">DARK</h3>
+      <a href="https://www.netflix.com/watch/81090319?tctx=2%2C2%2C%2C%2C%2C" target="_blank"><button>Watch Now</button>
+      </a>
+    </div>
+  </div>
+</div>
+</>
+ ,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
